42c3f27285eee74da045270f07b347188303a515
ecssiah <ecssiah@gmail.com>
2018-12-13 22:04:59 -0600

    Fine tune the view limits

--- src/constants/RenderConstants.cpp
+++ src/constants/RenderConstants.cpp
@@ -4,7 +4,7 @@ const unsigned SCREEN_SIZE_X{800};
 const unsigned SCREEN_SIZE_Y{600};
 const unsigned HALF_SCREEN_SIZE_X{SCREEN_SIZE_X / 2};
 const unsigned HALF_SCREEN_SIZE_Y{SCREEN_SIZE_Y / 2};
-const unsigned VIEW_X{6};
+const unsigned VIEW_X{7};
 const unsigned VIEW_Y{5};
 
 const float ASPECT_RATIO{(float)SCREEN_SIZE_X / (float)SCREEN_SIZE_Y};
