35e91065cc44bb9da24e0c9a0406166428b58d2d
ecssiah <ecssiah@gmail.com>
2019-01-05 16:31:59 -0600

    Rewirte message system using a much cleaner approach

--- include/utility/Log.h
+++ include/utility/Log.h
@@ -10,10 +10,12 @@ struct Log
     : changed{true}
     , msgs{}
   {
-    msgs.push_back("F");
-    msgs.push_back("I am a long string waiting to broken apart like a good monopoly would.");
-    msgs.push_back("1F");
-    msgs.push_back(".C");
+    msgs.push_back("A test message for the ages...");
+    msgs.push_back(
+      "I am a long string waiting to be broken apart like a good monopoly would."
+    );
+    msgs.push_back("Wrapping and wrapping and warping and warping.");
+    msgs.push_back("My name is Michael Chapman. This is a test, but only a test for the ages.");
 
   }
 
