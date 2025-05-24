ad0141ac5f19f74bd0d4baf4ac49b946629cad9f
ecssiah <ecssiah@gmail.com>
2025-05-24 18:32:31 +0800

    include aabb tests

---
+++ tests/aabb_intersection.rs
@@ -0,0 +1,157 @@
+use glam::Vec3;
+use last_ditch::simulation::{physics::aabb::AABB, BLOCK_SIZE};
+
+struct IntersectsAABBTestCase {
+    aabb: AABB,
+    expected_intersects_result: bool,
+}
+
+#[test]
+fn blocks_intersect_when_overlapping() {
+    let aabb = AABB::new(Vec3::new(0.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE));
+
+    let test_cases = vec![
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-0.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, -0.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, 0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, -0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.5, 0.5, 0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-0.5, -0.5, -0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+    ];
+
+    for test_case in test_cases {
+        let intersection_result = aabb.intersects(&test_case.aabb);
+
+        assert_eq!(
+            intersection_result, test_case.expected_intersects_result,
+            "Failed at {:?}",
+            test_case.aabb
+        );
+    }
+}
+
+#[test]
+fn blocks_intersect_when_intersecting() {
+    let aabb = AABB::new(Vec3::new(0.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE));
+
+    let test_cases = vec![
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 1.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, -1.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, 1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, -1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.0, 1.0, 1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.0, -1.0, -1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: true,
+        },
+    ];
+
+    for test_case in test_cases {
+        let intersection_result = aabb.intersects(&test_case.aabb);
+
+        assert_eq!(
+            intersection_result, test_case.expected_intersects_result,
+            "Failed at {:?}",
+            test_case.aabb
+        );
+    }
+}
+
+#[test]
+fn blocks_do_not_intersect_when_separated() {
+    let aabb = AABB::new(Vec3::new(0.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE));
+
+    let test_cases = vec![
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 1.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, -1.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, 1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, -1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.5, 1.5, 1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+        IntersectsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.5, -1.5, -1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_intersects_result: false,
+        },
+    ];
+
+    for test_case in test_cases {
+        let intersection_result = aabb.intersects(&test_case.aabb);
+
+        assert_eq!(
+            intersection_result, test_case.expected_intersects_result,
+            "Failed at {:?}",
+            test_case.aabb
+        );
+    }
+}
---
+++ tests/aabb_overlapping.rs
@@ -0,0 +1,157 @@
+use glam::Vec3;
+use last_ditch::simulation::{physics::aabb::AABB, BLOCK_SIZE};
+
+struct OverlapsAABBTestCase {
+    aabb: AABB,
+    expected_overlap_result: bool,
+}
+
+#[test]
+fn blocks_overlap_when_overlapping() {
+    let aabb = AABB::new(Vec3::new(0.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE));
+
+    let test_cases = vec![
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-0.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, -0.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, 0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, -0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.5, 0.5, 0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-0.5, -0.5, -0.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: true,
+        },
+    ];
+
+    for test_case in test_cases {
+        let overlaps_result = aabb.overlaps(&test_case.aabb);
+
+        assert_eq!(
+            overlaps_result, test_case.expected_overlap_result,
+            "Failed at {:?}",
+            test_case.aabb
+        );
+    }
+}
+
+#[test]
+fn blocks_do_not_overlap_when_intersecting() {
+    let aabb = AABB::new(Vec3::new(0.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE));
+
+    let test_cases = vec![
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 1.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, -1.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, 1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, -1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.0, 1.0, 1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.0, -1.0, -1.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+    ];
+
+    for test_case in test_cases {
+        let overlaps_result = aabb.overlaps(&test_case.aabb);
+
+        assert_eq!(
+            overlaps_result, test_case.expected_overlap_result,
+            "Failed at {:?}",
+            test_case.aabb
+        );
+    }
+}
+
+#[test]
+fn blocks_do_not_overlap_when_separated() {
+    let aabb = AABB::new(Vec3::new(0.0, 0.0, 0.0), Vec3::splat(BLOCK_SIZE));
+
+    let test_cases = vec![
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.5, 0.0, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 1.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, -1.5, 0.0), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, 1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(0.0, 0.0, -1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(1.5, 1.5, 1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+        OverlapsAABBTestCase {
+            aabb: AABB::new(Vec3::new(-1.5, -1.5, -1.5), Vec3::splat(BLOCK_SIZE)),
+            expected_overlap_result: false,
+        },
+    ];
+
+    for test_case in test_cases {
+        let overlaps_result = aabb.intersects(&test_case.aabb);
+
+        assert_eq!(
+            overlaps_result, test_case.expected_overlap_result,
+            "Failed at {:?}",
+            test_case.aabb
+        );
+    }
+}
