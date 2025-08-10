7549841c948ae57d19d5428a40a4a2c120300297
ecssiah <ecssiah@gmail.com>
2025-08-10 21:03:05 +0800

    extend WorldRayIter impl

--- src/simulation/state/world/grid/world_ray_iter.rs
+++ src/simulation/state/world/grid/world_ray_iter.rs
@@ -7,20 +7,24 @@ use glam::{IVec3, Vec3};
 
 pub struct WorldRayIter<'w> {
     world: &'w World,
+    done: bool,
     t: f32,
     origin: Vec3,
     direction: Vec3,
-    max_t: f32,
+    t_max: f32,
     position: IVec3,
-    step: IVec3,
+    step_direction: IVec3,
     t_remaining: Vec3,
     t_delta: Vec3,
-    done: bool,
 }
 
 impl<'w> WorldRayIter<'w> {
-    /// Walk voxels intersected by origin + t * direction for t ∈ [0, distance], world-bounds clipped.
-    pub fn new(world: &'w World, origin: Vec3, direction: Vec3, distance: f32) -> Option<Self> {
+    pub fn from_ray(
+        world: &'w World,
+        origin: Vec3,
+        direction: Vec3,
+        distance: f32,
+    ) -> Option<Self> {
         if !direction.is_finite() || direction == Vec3::ZERO || distance <= 0.0 {
             return None;
         }
@@ -29,10 +33,9 @@ impl<'w> WorldRayIter<'w> {
         let block_size = grid.block_size;
         let block_extent = grid.block_extent;
 
-        let world_center = Vec3::ZERO;
-        let world_aabb = AABB::new(world_center, Vec3::splat(grid.world_size_units));
+        let world_aabb = AABB::new(Vec3::ZERO, Vec3::splat(grid.world_size_units));
 
-        let (mut t0, mut t1) = ray_box_slab(origin, direction, &world_aabb);
+        let (mut t0, mut t1) = slab_test(origin, direction, &world_aabb);
 
         if !t0.is_finite() || !t1.is_finite() {
             return None;
@@ -45,13 +48,14 @@ impl<'w> WorldRayIter<'w> {
             return None;
         }
 
-        let epsilon = 1e-4;
-        let t_start = (t0 + epsilon).min(t1);
-        let world_position = origin + direction * t_start;
+        let t_start = t0;
+
+        let epsilon = 1e-6;
+        let world_position = origin + direction * t0 - direction.signum() * epsilon;
 
         let position = Grid::world_to_position(&world.grid, world_position);
 
-        let (sx, tdx, tx) = dda_axis_setup(
+        let (step_direction_x, t_delta_x, t_remaining_x) = dda_axis_setup(
             position.x,
             world_position.x,
             direction.x,
@@ -59,7 +63,7 @@ impl<'w> WorldRayIter<'w> {
             block_size,
         );
 
-        let (sy, tdy, ty) = dda_axis_setup(
+        let (step_direction_y, t_delta_y, t_remaining_y) = dda_axis_setup(
             position.y,
             world_position.y,
             direction.y,
@@ -67,7 +71,7 @@ impl<'w> WorldRayIter<'w> {
             block_size,
         );
 
-        let (sz, tdz, tz) = dda_axis_setup(
+        let (step_direction_z, t_delta_z, t_remaining_z) = dda_axis_setup(
             position.z,
             world_position.z,
             direction.z,
@@ -75,22 +79,24 @@ impl<'w> WorldRayIter<'w> {
             block_size,
         );
 
-        let step = IVec3::new(sx, sy, sz);
-        let t_remaining = Vec3::new(tx, ty, tz);
-        let t_delta = Vec3::new(tdx, tdy, tdz);
+        let step_direction = IVec3::new(step_direction_x, step_direction_y, step_direction_z);
+        let t_delta = Vec3::new(t_delta_x, t_delta_y, t_delta_z);
+        let t_remaining = Vec3::new(t_remaining_x, t_remaining_y, t_remaining_z);
 
-        Some(Self {
+        let world_ray_iter = Self {
             world,
+            done: false,
             t: t_start,
             origin,
             direction,
-            max_t: t1,
+            t_max: t1,
             position,
-            step,
+            step_direction,
             t_delta,
             t_remaining,
-            done: false,
-        })
+        };
+
+        Some(world_ray_iter)
     }
 }
 
@@ -113,27 +119,28 @@ impl<'w> Iterator for WorldRayIter<'w> {
                 return None;
             }
 
-            let (axis, step_axis, tnext) = min3_axis(self.t_remaining, self.step);
+            let (axis, step_direction_axis, t_next) =
+                get_next_block_boundary_info(self.step_direction, self.t_remaining);
 
-            if !tnext.is_finite() || tnext > self.max_t {
+            if !t_next.is_finite() || t_next > self.t_max {
                 self.done = true;
                 return None;
             }
 
-            self.t = tnext;
+            self.t = t_next;
 
             match axis {
-                0 => {
+                grid::Axis::X => {
                     self.t_remaining.x += self.t_delta.x;
-                    self.position.x += step_axis;
+                    self.position.x += step_direction_axis;
                 }
-                1 => {
+                grid::Axis::Y => {
                     self.t_remaining.y += self.t_delta.y;
-                    self.position.y += step_axis;
+                    self.position.y += step_direction_axis;
                 }
-                _ => {
+                grid::Axis::Z => {
                     self.t_remaining.z += self.t_delta.z;
-                    self.position.z += step_axis;
+                    self.position.z += step_direction_axis;
                 }
             }
 
@@ -142,20 +149,22 @@ impl<'w> Iterator for WorldRayIter<'w> {
                 return None;
             }
 
-            let enter_face_direction = face_from_axis_step(axis, step_axis);
+            let enter_face_direction = get_face_direction(axis, step_direction_axis);
 
             let (chunk_id, block_id) = Grid::position_to_ids(grid, self.position);
 
             let world_position = self.origin + self.direction * self.t;
 
-            return Some(BlockSample {
+            let block_sample = BlockSample {
                 t: self.t,
                 position: self.position,
                 world_position,
                 chunk_id,
                 block_id,
                 enter_face_direction,
-            });
+            };
+
+            return Some(block_sample);
         }
     }
 }
@@ -163,10 +172,10 @@ impl<'w> Iterator for WorldRayIter<'w> {
 /* ================= helpers (private) ================= */
 
 #[inline]
-fn ray_box_slab(origin: Vec3, direction: Vec3, slab_aabb: &AABB) -> (f32, f32) {
-    let (tx0, tx1) = axis_slab(origin.x, direction.x, slab_aabb.min.x, slab_aabb.max.x);
-    let (ty0, ty1) = axis_slab(origin.y, direction.y, slab_aabb.min.y, slab_aabb.max.y);
-    let (tz0, tz1) = axis_slab(origin.z, direction.z, slab_aabb.min.z, slab_aabb.max.z);
+fn slab_test(origin: Vec3, direction: Vec3, aabb: &AABB) -> (f32, f32) {
+    let (tx0, tx1) = slab_test_axis(origin.x, direction.x, aabb.min.x, aabb.max.x);
+    let (ty0, ty1) = slab_test_axis(origin.y, direction.y, aabb.min.y, aabb.max.y);
+    let (tz0, tz1) = slab_test_axis(origin.z, direction.z, aabb.min.z, aabb.max.z);
 
     let t_enter = tx0.max(ty0).max(tz0);
     let t_exit = tx1.min(ty1).min(tz1);
@@ -179,18 +188,24 @@ fn ray_box_slab(origin: Vec3, direction: Vec3, slab_aabb: &AABB) -> (f32, f32) {
 }
 
 #[inline]
-fn axis_slab(o: f32, d: f32, mn: f32, mx: f32) -> (f32, f32) {
-    if d == 0.0 {
-        if o < mn || o > mx {
+fn slab_test_axis(
+    origin_axis: f32,
+    direction_axis: f32,
+    min_face_axis: f32,
+    max_face_axis: f32,
+) -> (f32, f32) {
+    if direction_axis == 0.0 {
+        if origin_axis < min_face_axis || origin_axis > max_face_axis {
             return (f32::NAN, f32::NAN);
         } else {
             return (f32::NEG_INFINITY, f32::INFINITY);
         }
     }
 
-    let inv = 1.0 / d;
-    let mut t0 = (mn - o) * inv;
-    let mut t1 = (mx - o) * inv;
+    let direction_axis_inverse = direction_axis.recip();
+
+    let mut t0 = (min_face_axis - origin_axis) * direction_axis_inverse;
+    let mut t1 = (max_face_axis - origin_axis) * direction_axis_inverse;
 
     if t0 > t1 {
         core::mem::swap(&mut t0, &mut t1);
@@ -201,83 +216,86 @@ fn axis_slab(o: f32, d: f32, mn: f32, mx: f32) -> (f32, f32) {
 
 #[inline]
 fn dda_axis_setup(
-    axis_position: i32,
-    axis_world_position: f32,
-    axis_direction: f32,
+    position_axis: i32,
+    world_position_axis: f32,
+    direction_axis: f32,
     block_extent: f32,
     block_size: f32,
 ) -> (i32, f32, f32) {
     use core::f32::INFINITY;
 
-    let step = if axis_direction > 0.0 {
+    let step_direction = if direction_axis > 0.0 {
         1
-    } else if axis_direction < 0.0 {
+    } else if direction_axis < 0.0 {
         -1
     } else {
         return (0, INFINITY, INFINITY);
     };
 
-    let next_face_world_position = (axis_position as f32)
-        + if step > 0 {
+    let next_face_world_position = (position_axis as f32)
+        + if step_direction > 0 {
             block_extent
         } else {
             -block_extent
         };
 
-    let t_max = (next_face_world_position - axis_world_position) / axis_direction;
-    let t_delta = block_size / axis_direction.abs();
+    let t_delta = block_size / direction_axis.abs();
+    let t_max = (next_face_world_position - world_position_axis) / direction_axis;
 
-    (step, t_delta, t_max)
+    (step_direction, t_delta, t_max)
 }
 
 #[inline]
-fn min3_axis(t_remaining: Vec3, step: IVec3) -> (u8, i32, f32) {
-    let txp = if step.x == 0 || !t_remaining.x.is_finite() {
+fn get_next_block_boundary_info(
+    step_direction: IVec3,
+    t_remaining: Vec3,
+) -> (grid::Axis, i32, f32) {
+    let txp = if step_direction.x == 0 || !t_remaining.x.is_finite() {
         f32::INFINITY
     } else {
         t_remaining.x
     };
 
-    let typ = if step.y == 0 || !t_remaining.y.is_finite() {
+    let typ = if step_direction.y == 0 || !t_remaining.y.is_finite() {
         f32::INFINITY
     } else {
         t_remaining.y
     };
 
-    let tzp = if step.z == 0 || !t_remaining.z.is_finite() {
+    let tzp = if step_direction.z == 0 || !t_remaining.z.is_finite() {
         f32::INFINITY
     } else {
         t_remaining.z
     };
 
-    let mut axis: u8 = 0;
-    let mut step_axis = step.x;
-    let mut tnext = txp;
+    let mut axis = grid::Axis::X;
+    let mut step_direction_axis = step_direction.x;
+    let mut t_next = txp;
 
-    if typ < tnext {
-        axis = 1;
-        step_axis = step.y;
-        tnext = typ;
+    if typ < t_next {
+        axis = grid::Axis::Y;
+        step_direction_axis = step_direction.y;
+        t_next = typ;
     }
 
-    if tzp < tnext {
-        axis = 2;
-        step_axis = step.z;
-        tnext = tzp;
+    if tzp < t_next {
+        axis = grid::Axis::Z;
+        step_direction_axis = step_direction.z;
+        t_next = tzp;
     }
 
-    (axis, step_axis, tnext)
+    (axis, step_direction_axis, t_next)
 }
 
 #[inline]
-fn face_from_axis_step(axis: u8, step: i32) -> grid::Direction {
-    match (axis, step) {
-        (0, 1) => grid::Direction::XnYoZo,
-        (0, -1) => grid::Direction::XpYoZo,
-        (1, 1) => grid::Direction::XoYnZo,
-        (1, -1) => grid::Direction::XoYpZo,
-        (2, 1) => grid::Direction::XoYoZn,
-        (2, -1) => grid::Direction::XoYoZp,
+fn get_face_direction(axis: grid::Axis, step_direction: i32) -> grid::Direction {
+    match (axis, step_direction) {
+        (grid::Axis::X, 1) => grid::Direction::XnYoZo,
+        (grid::Axis::X, -1) => grid::Direction::XpYoZo,
+        (grid::Axis::Y, 1) => grid::Direction::XoYnZo,
+        (grid::Axis::Y, -1) => grid::Direction::XoYpZo,
+        (grid::Axis::Z, 1) => grid::Direction::XoYoZn,
+        (grid::Axis::Z, -1) => grid::Direction::XoYoZp,
         _ => panic!("Requesting face for non-axis direction"),
     }
 }
